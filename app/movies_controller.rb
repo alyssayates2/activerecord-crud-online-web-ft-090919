34d46a5f0155440247c779dd93f8fc5a6b7ecdea
Alyssa Yates <bosslys@icloud.com>
2019-10-28 18:58:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -51,12 +51,20 @@ def can_find_the_first_item_from_the_database_using_id
   Movie.find(1)
 end
 
-def can_find_by_multiple_attributes
+def can_find_by_multiple_attributes(hash)
   # Search Values:
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+    column = hash.keys[0]
+    sql = <<-SQL
+    SELECT *
+    FROM movies
+    WHERE #{column} = ?
+    SQL
+    DB[:conn].execute(sql, hash[column])
+
+  end
 end
 
 def can_find_using_where_clause_and_be_sorted
