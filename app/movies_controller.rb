e2a686659d02e39906a4879f08777b38e73ebf3a
Alyssa Yates <bosslys@icloud.com>
2019-10-28 17:23:12 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,8 +30,8 @@ end
   def can_be_created_in_a_block(args = {title: "The Room", release_date: 2003})
     binding.pry
     Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = args[:title]
+    m.release_date = args[:release_date]
     end
   end
 
