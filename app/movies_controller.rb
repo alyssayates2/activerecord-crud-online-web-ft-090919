650421063802de6bf83590522948e987e0782358
Alyssa Yates <bosslys@icloud.com>
2019-10-28 20:15:27 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -63,8 +63,8 @@ end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  movie = Movie.update(title: "Awesome Flick")
-  movie.title = "Even Awesomer Flick"
+  Movie.update(title: "Awesome Flick")
+  Movie.title = "Even Awesomer Flick"
   movie.save
 end
 
