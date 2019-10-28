3f7d7b6776d8db1bc10fbcd304b21e813f74d61d
Alyssa Yates <bosslys@icloud.com>
2019-10-28 18:47:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,7 +48,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  SELECT * FROM movies WHERE (clients.id = 0) LIMIT 1
+  SELECT * FROM movies WHERE (movies.id = 0) LIMIT 1
 end
 
 def can_find_by_multiple_attributes
