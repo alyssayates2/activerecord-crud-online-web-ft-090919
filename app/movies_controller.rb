1326395c979ba71c51b1895be90ca30197f043e6
Alyssa Yates <bosslys@icloud.com>
2019-10-28 17:10:44 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,6 +28,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
   def can_be_created_in_a_block(args = {title: "The Room", release_date: "2003"})
+    binding.pry
     Movie.create do |m|
     m.title = "The Room"
     m.release_date = 2003
