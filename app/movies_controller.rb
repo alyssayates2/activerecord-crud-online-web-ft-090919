0de3e88424f372c56be9bea1c74a0ca17f019b95
Alyssa Yates <bosslys@icloud.com>
2019-10-28 19:08:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -51,12 +51,14 @@ def can_find_the_first_item_from_the_database_using_id
   Movie.find(1)
 end
 
-def can_find_by_multiple_attributes
+def can_find_by_multiple_attributes(hash)
+    # binding.pry
+    column = hash.keys[0]
   # Search Values:
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-    Movie.find_by!
+    Movie.find_by!(hash)
 end
 
 def can_find_using_where_clause_and_be_sorted
